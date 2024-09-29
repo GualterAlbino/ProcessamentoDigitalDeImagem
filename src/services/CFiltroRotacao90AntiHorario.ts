4b1111159fe50c638cb1e4d067e73a38b792ef03
jvitortei45 <jvitortei45@gmail.com>
2024-09-29 16:31:29 -0300

    Correção das Rotações em 90 em ambos os sentidos

--- src/services/CFiltroRotacao90AntiHorario.ts
+++ src/services/CFiltroRotacao90AntiHorario.ts
@@ -1,5 +1,23 @@
 export default class CFiltroRotacao90AntiHorario implements IFiltro {
   executar(pImagem: number[][]): number[][] {
-    throw new Error('Method not implemented.')
+    try {
+      const linhas = pImagem.length
+      const colunas = pImagem[0].length
+      const matrizRotacionada: number[][] = Array.from({ length: colunas }, () => [])
+    
+      // Rotaciona a matriz 90 graus no sentido anti-horário
+      for (let i = 0; i < linhas; i++) {
+        for (let j = 0; j < colunas; j++) {
+          // Copia os pixels para a nova posição
+          matrizRotacionada[colunas - 1 - j][i] = pImagem[i][j]
+        }
+      }
+    
+      return matrizRotacionada
+    } catch (error) {
+      console.error(error)
+      throw error
+    }
+    
   }
 }
--- src/services/CFiltroRotacao90Horario.ts
+++ src/services/CFiltroRotacao90Horario.ts
@@ -1,19 +1,23 @@
 export default class CFiltroRotacao90Horario implements IFiltro {
   public executar(pImagem: number[][]): number[][] {
     try {
-      const matriz = pImagem
-      const matrizRotacionada: number[][] = []
-
-      for (let i = 0; i < matriz.length; i++) {
-        for (let j = 0; j < matriz[i].length; j++) {
-          matrizRotacionada[j] = matrizRotacionada[j] || [] // cria a linha se ainda não existir
-          matrizRotacionada[j][i] = matriz[i][j] // copia o pixel
+      const linhas = pImagem.length
+      const colunas = pImagem[0].length
+      const matrizRotacionada: number[][] = Array.from({ length: colunas }, () => [])
+    
+      // Rotaciona a matriz 90 graus no sentido horário
+      for (let i = 0; i < linhas; i++) {
+        for (let j = 0; j < colunas; j++) {
+          // Copia os pixels para a nova posição
+          matrizRotacionada[j][linhas - 1 - i] = pImagem[i][j]
         }
       }
+    
       return matrizRotacionada
     } catch (error) {
       console.error(error)
       throw error
     }
+    
   }
 }
