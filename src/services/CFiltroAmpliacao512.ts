7c3d04cb214442e550c03957211bde9119b003a2
jvitortei45 <jvitortei45@gmail.com>
2024-09-29 15:26:41 -0300

    Filtros de Ampliação por replicação 512x512 e 1024x1024

--- src/services/CFiltroAmpliacao512.ts
+++ src/services/CFiltroAmpliacao512.ts
@@ -1,5 +1,33 @@
 export default class CFiltroAmpliacao512 implements IFiltro {
   executar(pImagem: number[][]): number[][] {
-    throw new Error('Method not implemented.')
+    try {
+
+      const alturaOriginal = pImagem.length;
+      const larguraOriginal = pImagem[0].length;
+
+      const matrizAmpliada: number[][] = Array.from(
+        { length: 512},
+        () => Array(512).fill(0) // Cria uma matriz de 512x512 com todos os pixels preenchidos com 0
+      )
+
+      for (let i = 0; i < alturaOriginal; i++) {
+        for (let j = 0; j < larguraOriginal; j++) {
+          const novaLinha = i * 2  // Cada pixel vai ser replicado em 2 pixels na nova imagem
+          const novaColuna = j * 2
+
+          // Preencehe os pixels da nova imagem
+          matrizAmpliada[novaLinha][novaColuna] = pImagem[i][j] 
+          matrizAmpliada[novaLinha][novaColuna + 1] = pImagem[i][j]
+          matrizAmpliada[novaLinha + 1][novaColuna] = pImagem[i][j]
+          matrizAmpliada[novaLinha + 1][novaColuna + 1] = pImagem[i][j]
+        }
+      }
+
+      return matrizAmpliada
+
+    } catch (error) {
+    console.error(error);
+    throw error;
+    }
   }
 }
