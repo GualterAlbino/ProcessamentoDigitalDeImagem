df8cfb96725f40fdc24eec007696bd739d1b0384
Gualter Albino <gualteralbino1000@gmail.com>
2024-10-07 17:08:42 -0300

    Filtro de Compressão

---
+++ src/services/CFiltroAmpliacaoBilinear1024.ts
@@ -0,0 +1,5 @@
+export default class CFiltroAmpliacaoBilinear1024 implements IFiltro {
+  executar(pImagem: number[][]): number[][] {
+    throw new Error('Method not implemented.')
+  }
+}
---
+++ src/services/CFiltroAmpliacaoBilinear512.ts
@@ -0,0 +1,5 @@
+export default class CFiltroAmpliacaoBilinear512 implements IFiltro {
+  executar(pImagem: number[][]): number[][] {
+    throw new Error('Method not implemented.')
+  }
+}
